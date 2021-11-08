ce6d7c831f57f1a49e0b1954289e80da0b5fbc4b
VincentETN <vincent8947@gmail.com>
2021-11-08 19:21:00 +0800

    simplify ProjectTest.java code

--- src/test/java/pvs/app/entity/ProjectTest.java
+++ src/test/java/pvs/app/entity/ProjectTest.java
@@ -7,36 +7,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import pvs.app.Application;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class ProjectTest {
     Project project;
-    Repository githubRepository;
-    Set<Repository> repositorySet;
+    final String testProjectName = "myProject";
 
     @Before
     public void setup() {
         project = new Project();
         project.setMemberId(1L);
-        project.setName("myProject");
-
-        githubRepository = new Repository();
-        githubRepository.setType("github");
-        githubRepository.setUrl("https://github.com/facebook/react");
-        githubRepository.setRepositoryId(1L);
-
-        repositorySet = new HashSet<>();
-        repositorySet.add(githubRepository);
-        project.setRepositorySet(repositorySet);
+        project.setName(testProjectName);
     }
 
     @Test
     public void getNameTest() {
-        assertEquals("myProject", project.getName());
+        assertEquals(testProjectName, project.getName());
     }
 }
