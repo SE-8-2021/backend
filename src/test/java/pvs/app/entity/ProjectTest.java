7e1dfecbd2321eceb631e2f0f707d66b83d89081
VincentETN <72587013+VincentETN@users.noreply.github.com>
2021-11-18 14:52:18 +0800

    refactor Project entity and add Project entity test (#9)
    * refactor Project and add Project test
    
    * Refactor Project and add Project test
    
    * simplify ProjectTest.java code
    
    * revise database password/ add deleted property to Project entity
    
    * revise test name in ProjectTest
    
    * use camel rule in test name
    
    * the deleted property is renamed as status
    
    * the status property of Project entity is renamed as removed
    
    * test data is renamed as givenProject in ProjectTest
    
    * delete the line setting memberId in ProjectTest setup
    
    * revise test name in ProjectTest
    
    * rename the test, removedShouldHaveDefaultValue
    
    Co-authored-by: TU-Lin <trusaidlin@gmail.com>

---
+++ src/test/java/pvs/app/entity/ProjectTest.java
@@ -0,0 +1,41 @@
+package pvs.app.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pvs.app.Application;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ProjectTest {
+    final String testProjectName = "myProject";
+    Project givenProject;
+
+    @Before
+    public void setup() {
+        givenProject = new Project();
+        givenProject.setName(testProjectName);
+    }
+
+    @Test
+    public void correctlyGetProjectName() {
+        assertEquals(testProjectName, givenProject.getName());
+    }
+
+    @Test
+    public void removedAttributeShouldHaveDefaultValue() {
+        assertFalse(givenProject.isRemoved());
+    }
+
+    @Test
+    public void setProjectAsRemoved() {
+        givenProject.setRemoved(true);
+        assertTrue(givenProject.isRemoved());
+    }
+}
