775343b1a3057bd7e78a93ecf0845ff54d00f024
VincentETN <vincent8947@gmail.com>
2022-01-10 16:52:42 +0800

    refactor: using just Getter and Setter instead of Data annotation

--- src/main/java/pvs/app/dto/CreateProjectDTO.java
+++ src/main/java/pvs/app/dto/CreateProjectDTO.java
@@ -1,8 +1,10 @@
 package pvs.app.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class CreateProjectDTO {
     private Long memberId;
     private String projectName;
