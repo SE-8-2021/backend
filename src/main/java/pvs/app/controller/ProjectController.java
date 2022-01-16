fe43dbd0942ecac058b24e6d67d7c83b5f074a0c
VincentETN <72587013+VincentETN@users.noreply.github.com>
2022-01-16 15:24:09 +0800

    Refactor adding repository (#49)
    * refactor: move check url api from ProjectController to RepositoryController
    
    * refactor: move add repository API from ProjectController to RepositoryController
    
    * refactor: move add repository API back to ProjectController and remove checkURL in add repo API
    
    * reset: keep checkURL in adding repository API
    
    Co-authored-by: TU-Lin <trusaidlin@gmail.com>

--- src/main/java/pvs/app/controller/ProjectController.java
+++ src/main/java/pvs/app/controller/ProjectController.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pvs.app.dto.*;
+import pvs.app.dto.AddRepositoryDTO;
+import pvs.app.dto.CreateProjectDTO;
+import pvs.app.dto.ResponseProjectDTO;
 import pvs.app.service.ProjectService;
 import pvs.app.service.RepositoryService;
 
@@ -31,58 +33,10 @@ public class ProjectController {
         this.repositoryService = repositoryService;
     }
 
-    @GetMapping("/repository/github/check")
-    public ResponseEntity<String> checkGitHubURL(@RequestParam("url") String url) {
-        if (repositoryService.checkGithubURL(url)) {
-            return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
-    }
-
-    @GetMapping("/repository/sonar/check")
-    public ResponseEntity<String> checkSonarURL(@RequestParam("url") String url) {
-        if (repositoryService.checkSonarURL(url)) {
-            return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
-    }
-
-    @GetMapping("/repository/gitlab/check")
-    public ResponseEntity<String> checkGitLabURL(@RequestParam("url") String url) {
-        if (repositoryService.checkGitLabURL(url)) {
-            return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
-    }
-
-    @GetMapping("/repository/trello/check")
-    public ResponseEntity<String> checkTrelloURL(@RequestParam("url") String url) {
-        if (repositoryService.checkTrelloURL(url)) {
-            return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
-    }
-
     @PostMapping("/project")
     public ResponseEntity<String> createProject(@RequestBody CreateProjectDTO createProjectDTO) {
         projectService.create(createProjectDTO);
         return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-    }
-
-    @PostMapping("/project/{projectId}/repository/sonar")
-    public ResponseEntity<String> addSonarRepository(@RequestBody AddRepositoryDTO addSonarRepositoryDTO) {
-        try {
-            if (repositoryService.checkSonarURL(addSonarRepositoryDTO.getRepositoryURL())) {
-                if (projectService.addSonarRepo(addSonarRepositoryDTO)) {
-                    return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-                }
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(failMessage);
-            }
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionMessage);
-        }
     }
 
     @PostMapping("/project/{projectId}/repository/github")
@@ -111,7 +65,22 @@ public class ProjectController {
             }
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
         } catch (Exception e) {
-            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@");
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionMessage);
+        }
+    }
+
+    @PostMapping("/project/{projectId}/repository/sonar")
+    public ResponseEntity<String> addSonarRepository(@RequestBody AddRepositoryDTO addSonarRepositoryDTO) {
+        try {
+            if (repositoryService.checkSonarURL(addSonarRepositoryDTO.getRepositoryURL())) {
+                if (projectService.addSonarRepo(addSonarRepositoryDTO)) {
+                    return ResponseEntity.status(HttpStatus.OK).body(successMessage);
+                }
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(failMessage);
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionMessage);
         }
